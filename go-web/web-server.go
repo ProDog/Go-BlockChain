d69d674a00f0b79aab9e1b4e8e1b26cdbf144823
ProDog <xiuboard@foxmail.com>
2019-12-24 14:56:14 +0800

    update

--- go-web/web-server.go
+++ go-web/web-server.go
@@ -5,6 +5,7 @@ import(
 	"net/http"
 	"strings"
 	"log"
+	"html/template"
 )
 
 func sayHelloName(w http.ResponseWriter, r *http.Request) {
@@ -26,11 +27,47 @@ func testFunc(w http.ResponseWriter,r *http.Request) {
 	fmt.Fprintf(w,"test!")
 }
 
+func login(w http.ResponseWriter,r *http.Request) {
+	fmt.Println("method:",r.Method)
+	if r.Method=="GET" {
+		t,_:=template.ParseFiles("login.gtpl")
+		log.Println(t.Execute(w,nil))
+	} else {
+		r.ParseForm()
+		
+		fmt.Println("username:",r.Form["username"])
+		fmt.Println("pwd:",r.Form["password"])
+	}
+}
+
 func main() {
 	http.HandleFunc("/",sayHelloName)
 	http.HandleFunc("/test",testFunc)
+	http.HandleFunc("/login",login)
 	err:=http.ListenAndServe(":9090",nil)
 	if err!=nil{
 		log.Fatal("ListenAndServe: ",err)
 	}
+
+	// mux :=&MyMux{}
+	// http.ListenAndServe(":9090",mux)
 }
+
+
+type MyMux struct {
+}
+
+func (p *MyMux) ServeHTTP(w http.ResponseWriter,r *http.Request) {
+	if r.URL.Path =="/" {
+		sayHello(w,r)
+		return
+	}
+
+	http.NotFound(w,r)
+	return
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w,"Hello my route!")
+}
+
